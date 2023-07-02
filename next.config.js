001d1cc08ee87d4e02ac78db58a48206f05c9694
Nicholas Romero <ncrmro@gmail.com>
2023-07-01 19:01:56 -0600

    feat: enable typed routes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+// @ts-check
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
   experimental: {
     serverActions: true,
+    typedRoutes: true,
   },
 };
+
+module.exports = nextConfig;
